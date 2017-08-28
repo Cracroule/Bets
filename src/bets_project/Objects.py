1ea7ce4c91419af5f23549647de542bd95d6c5ab
cracr <cracroule@gmail.com>
2017-08-28 23:06:56 +0200

    implement load matches by season from csv

--- src/bets_project/Objects.py
+++ src/bets_project/Objects.py
@@ -77,13 +77,17 @@ class Match(BetObject):
                    self.home_team == other.home_team and self.away_team == other.away_team
         return NotImplemented
 
+    def __hash__(self):
+        return hash((self.competition_season, self.date, self.home_team, self.away_team))
+
 
 class MatchResult(BetObject):
 
-    def __init__(self, **kwargs):
-        self.home_goals = kwargs['home_goals']
-        self.away_goals = kwargs['away_goals']
-        self.match = kwargs['match']
+    def __init__(self, match, home_goals, away_goals):
+        self.match = match
+        self.home_goals = home_goals
+        self.away_goals = away_goals
+
 
     def __str__(self):
         return '%s: %d - %d' % (str(self.match), int(self.home_goals), int(self.away_goals))
@@ -92,6 +96,9 @@ class MatchResult(BetObject):
         if isinstance(other, self.__class__):
             return self.match == other.match
         return NotImplemented
+
+    def __hash__(self):
+        return hash((self.home_goals, self.away_goals, self.match))
 
 
 class Team(BetObject):
@@ -108,6 +115,9 @@ class Team(BetObject):
             return self.name == other.name
         return NotImplemented
 
+    def __hash__(self):
+        return hash(self.name)
+
 
 class Bookmaker(BetObject):
 
@@ -123,14 +133,17 @@ class Bookmaker(BetObject):
             return self.name == other.name and self.label == other.label
         return NotImplemented
 
+    def __hash__(self):
+        return hash((self.name, self.label))
+
 
 class Event(BetObject):
 
     LABEL_LIST = ['H', 'D', 'A']
 
-    def __init__(self, **kwargs):
-        self.label = kwargs['label']
-        self.description = kwargs['description']
+    def __init__(self, label, description=None):
+        self.label = label
+        self.description = description
 
     def __str__(self):
         return self.description
@@ -140,14 +153,17 @@ class Event(BetObject):
             return self.label == other.label
         return NotImplemented
 
+    def __hash__(self):
+        return hash(self.label)
+
 
 class EventOdds(BetObject):
 
-    def __init__(self, **kwargs):
-        self.bookmaker = kwargs['bookmaker']
-        self.event = kwargs['event']
-        self.match = kwargs['match']
-        self.value = kwargs['value']
+    def __init__(self, bookmaker, match, event, value):
+        self.bookmaker = bookmaker
+        self.match = match
+        self.event = event
+        self.value = value
 
     def __str__(self):
         return '%s for %s by %s = %s' % (self.event, self.match, self.bookmaker, self.value)
@@ -156,3 +172,6 @@ class EventOdds(BetObject):
         if isinstance(other, self.__class__):
             return self.match == other.match and self.event == other.event and self.bookmaker == other.bookmaker
         return NotImplemented
+
+    def __hash__(self):
+        return hash((self.match, self.event, self.bookmaker))
--- src/bets_project/ObjectsManager.py
+++ src/bets_project/ObjectsManager.py
@@ -1,5 +1,8 @@
-from bets_project.Objects import Sport, Competition, CompetitionSeason
+from bets_project.Objects import Sport, Competition, CompetitionSeason, EventOdds, Event, \
+    Team, Match, MatchResult, Bookmaker, BetObject
 import inspect
+import datetime
+import csv
 
 
 class ObjectsManager(object):
@@ -8,19 +11,125 @@ class ObjectsManager(object):
         self.cache = dict()
 
     def push(self, obj, allow_iterable=True):
-        try:
-            for o in obj:
-                self.push(o, allow_iterable=False)
-        except:
-            pass
+        if allow_iterable:
+            try:
+                for o in obj:
+                    self.push(o, allow_iterable=False)
+            except TypeError:
+                pass
         obj_cl = obj.__class__
         if obj_cl not in self.cache.keys():
             self.cache[obj_cl] = set()
         self.cache[obj_cl].add(obj)
 
     def get_all(self, cl):
-        if not inspect.isclass(cl):
-            return Exception("get_all should be called with an arg of type class")
+        if not issubclass(cl, BetObject):
+            return Exception("get_all should be called with an arg of type BetObject")
         if cl in self.cache:
             return self.cache[cl]
         return []
+
+    # return the already instanciated object if existing, else push it and return itself
+    #needed ?
+    # def get_or_create(self, obj):
+    #     obj_cl = obj.__class__
+    #     if obj_cl not in self.cache.keys():
+    #         self.cache[obj_cl] = set()
+    #         self.cache[obj_cl].add(obj)
+    #         return obj
+    #     else:
+    #         if obj in self.cache[obj_cl]:
+    #             return
+
+    def init(self):
+        self.register_ligue_1()
+        self.register_odds_structure()
+
+    def register_ligue_1(self):
+        # sports creation
+        football = Sport('Football')
+        self.push(football)
+
+        # competition creation
+        ligue_1 = Competition("Ligue 1", football)
+        self.push(ligue_1)
+
+        return ligue_1
+
+    def register_odds_structure(self):
+
+        odd_event_list = [("H", "Home Win Odds"),
+                          ("D", "Draw Odds"),
+                          ("A", "Away Win Odds"), ]
+
+        for e in odd_event_list:
+            event = Event(label=e[0], description=e[1])
+            self.push(event)
+
+        bookmakers_list = [('Bet365', 'B365'),
+                           ('Blue Square', 'BS'),
+                           ('Bet and Win', 'BW'),
+                           ('Gamebookers', 'GB'),
+                           ('Interwetten', 'IW'),
+                           ('Ladbrokes', 'LB'),
+                           ('Pinnacle', 'PS'),
+                           ('Sporting', 'SO'),
+                           ('Sportingbet', 'SB'),
+                           ('Stan James', 'SJ'),
+                           ('Stanleybet', 'SY'),
+                           ('VC Bet', 'VC'),
+                           ('William Hill', 'WH'),
+                           ('BetBrain average', 'BbAv'), ]
+
+        self.push((Bookmaker(e[0], e[1]) for e in bookmakers_list))
+        # for bkm in bookmakers_list:
+        #     bookmaker = Bookmaker(name=bkm[0], label=bkm[1])
+        #     self.push(bookmaker)
+
+    """ Create and register a match. If necessary, creates involved teams
+    Input is a dict with following keys:
+    - HomeTeam
+    - AwayTeam
+    - season
+    - Date
+    - FTHG
+    - FTAG
+    """
+    # stats might be saved here as well, ToBeImplemented later on
+    # TODO: implement stats
+    def register_match(self, input_dict):
+        home_team = Team(name=input_dict['HomeTeam'])
+        away_team = Team(name=input_dict['AwayTeam'])
+        self.push(home_team)
+        self.push(away_team)
+
+        match_date = datetime.datetime.strptime(input_dict['Date'], '%d/%m/%y').date()
+        match = Match(competition_season=input_dict['competition_season'], date=match_date, home_team=home_team,
+                      away_team=away_team)
+        match_result = MatchResult(match, int(input_dict['FTHG']), int(input_dict['FTAG']))
+        self.push(match)
+        self.push(match_result)
+
+        # save odds
+        # all_bkmk = ['BbAv'] # not interested by other ones for now
+        all_bkmk = (bkm for bkm in self.get_all(Bookmaker) if bkm.label == 'BbAv')
+        # all_bkmk = list(self.get_all(Bookmaker))
+        for bkmk in all_bkmk:
+            odd_labels = [bkmk.label + end_label for end_label in ('H', 'D', 'A')]
+            for odd_label in odd_labels:
+                if odd_label in input_dict.keys():
+                    event = Event(odd_label[-1])
+                    event_odds = EventOdds(bkmk, match, event, value=input_dict[odd_label])
+                    self.push(event_odds)
+
+    def register_full_season_matches_from_csv(self, competition_season, file):
+        # data_dir = 'data/'
+        # competition_label = competition.name.low().replace(' ', '').replace('_', '')
+        # season_label = season_tag.replace('/', '_').replace('-', '_')
+        # file = data_dir + competition_label + season_label + '.csv'
+        with open(file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row_dict in reader:
+                row_dict['competition_season'] = competition_season
+                self.register_match(row_dict)
+
